6c5ebd8482ce22f64a935ef4d73091c94557c124
jjgorton <jjgorton@gmail.com>
2020-02-27 16:59:17 -0500

    fixed nav scroll bug and firefox display bug

--- nav.js
+++ nav.js
@@ -7,92 +7,33 @@ let dir = window.matchMedia('(min-width: 700px)');
 const check = media => (media.matches ? 'X' : 'X');
 dir.addListener(check);
 
-const scroll = document.querySelector('.scroll-position');
-
-const navItems = document.querySelectorAll('.nav-item');
-
-const about = document.getElementById('about');
-const aboutT = about.getBoundingClientRect().top;
-const aboutB = about.getBoundingClientRect().bottom;
-
-const skills = document.getElementById('skills');
-const skillsT = skills.getBoundingClientRect().top;
-const skillsB = skills.getBoundingClientRect().bottom;
-
-const projects = document.getElementById('projects');
-const projectsT = projects.getBoundingClientRect().top;
-const projectsB = projects.getBoundingClientRect().bottom;
-
-const contact = document.getElementById('contact');
-const contactT = contact.getBoundingClientRect().top;
-const contactB = contact.getBoundingClientRect().bottom;
-
-navItems[3].style.color = 'blue';
-
 window.onscroll = e => {
-    // console.log(check(dir));
-    // console.log('outer', navItems[3].style.color, navItems[3]);
-    let position = window.scrollY;
-    // console.log('pos', position);
-    // let height =
-    //     document.scrollingElement.scrollHeight -
-    //     document.scrollingElement.clientHeight;
+    const scroll = document.querySelector('.scroll-position');
 
-    // let width = document.scrollingElement.clientWidth;
-    // let percent = (position / height) * 300;
+    // let zoom = window.devicePixelRatio;
+
+    const about = document.getElementById('about');
+    const aboutB = about.getBoundingClientRect().bottom;
+
+    const skills = document.getElementById('skills');
+    const skillsB = skills.getBoundingClientRect().bottom;
+
+    const projects = document.getElementById('projects');
+    const projectsB = projects.getBoundingClientRect().bottom;
+
+    const winHeight = window.innerHeight / 2;
 
     // check(dir) returns 'X' or 'Y' depending on screen size
-    // adding or subtracting integers for minor adjustment of scroll positions
-    // console.log('about', aboutT, aboutB);
-    if (position > aboutT && position < aboutB - 50) {
-        // console.log('about if');
+    if (aboutB > winHeight) {
         scroll.style.transform = `translate${check(dir)}(${0}%)`;
-        // navItems[0].style.color = '#1f2833';
     }
-    // console.log('skills', skillsT, skillsB);
-    if (position > skillsT - 50 && position < skillsB - 50) {
-        // console.log('skills if');
+    if (aboutB < winHeight && skillsB > winHeight) {
         scroll.style.transform = `translate${check(dir)}(${100}%)`;
-        // navItems[1].style.color = '#1f2833';
     }
-    // console.log('project', projectsT, projectsB);
-    if (position > projectsT - 50 && position < contactT + contactT * 0.3) {
-        // console.log('proj if');
+    if (skillsB < winHeight && projectsB > winHeight) {
         scroll.style.transform = `translate${check(dir)}(${200}%)`;
-        // navItems[2].style.color = '#1f2833';
     }
-    // console.log('contact', contactT + contactT * 0.3, contactB);
-    if (position > contactT + contactT * 0.3) {
-        // console.log('contact if');
+    if (projectsB < winHeight) {
         scroll.style.transform = `translate${check(dir)}(${300}%)`;
-        // navItems[3].style.color = '#1f2833';
-        // console.log(contact.style.color);
     }
 };
-
-// const navItem = document.querySelectorAll('.nav-item');
-// navItem.forEach((item, i) => {
-//     item.addEventListener('click', () => {
-//         navIndicator(item, i);
-//     });
-// });
-
-// function grabInside(element) {
-//     let str = element.innerHTML;
-// }
-
-// function navIndicator(item, index) {
-//     let str = item.innerHTML.split('"')[1];
-//     let section = document.querySelector(str);
-//     let itemPos = section.getBoundingClientRect().top;
-
-//     window.onscroll = () => {
-//         let position = window.scrollY;
-//         let diff = itemPos - position;
-//         console.log('click', index, itemPos, position, diff);
-//         // scroll.style.transform = `translateX(${index * 100}%)`;
-//         if (diff < 5 && diff > -5) {
-//             scroll.style.transform = `translateX(${index * 100}%)`;
-//         }
-//     };
-// }
