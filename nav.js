f81a362442e4c6d373002d203d20a1578bc079b2
jjgorton <jjgorton@gmail.com>
2020-07-27 16:30:17 -0400

    color updates, nav updates

--- nav.js
+++ nav.js
@@ -4,10 +4,10 @@
 // this is to check if the indicator needs to translate Y or X
 let dir = window.matchMedia('(min-width: 700px)');
 // putting the nav back at the bottom for all screen sizes !('Y' : 'X')
-const check = media => (media.matches ? 'X' : 'X');
+const check = (media) => (media.matches ? 'X' : 'X');
 dir.addListener(check);
 
-window.onscroll = e => {
+window.onscroll = (e) => {
     const scroll = document.querySelector('.scroll-position');
 
     // let zoom = window.devicePixelRatio;
@@ -15,11 +15,20 @@ window.onscroll = e => {
     const about = document.getElementById('about');
     const aboutB = about.getBoundingClientRect().bottom;
 
-    const skills = document.getElementById('skills');
-    const skillsB = skills.getBoundingClientRect().bottom;
+    // const skills = document.getElementById('skills');
+    // const skillsB = skills.getBoundingClientRect().bottom;
 
-    const projects = document.getElementById('projects');
-    const projectsB = projects.getBoundingClientRect().bottom;
+    // const projects = document.getElementById('projects');
+    // const projectsB = projects.getBoundingClientRect().bottom;
+
+    const coachMe = document.getElementById('coachMe');
+    const coachMeB = coachMe.getBoundingClientRect().bottom;
+
+    const noiseController = document.getElementById('noiseController');
+    const noiseControllerB = noiseController.getBoundingClientRect().bottom;
+
+    const deforestDash = document.getElementById('deforestDash');
+    const deforestDashB = deforestDash.getBoundingClientRect().bottom;
 
     const winHeight = window.innerHeight / 2;
 
@@ -27,13 +36,22 @@ window.onscroll = e => {
     if (aboutB > winHeight) {
         scroll.style.transform = `translate${check(dir)}(${0}%)`;
     }
-    if (aboutB < winHeight && skillsB > winHeight) {
+    // if (aboutB < winHeight && skillsB > winHeight) {
+    //     scroll.style.transform = `translate${check(dir)}(${100}%)`;
+    // }
+    // if (skillsB < winHeight && projectsB > winHeight) {
+    //     scroll.style.transform = `translate${check(dir)}(${200}%)`;
+    // }
+    if (aboutB < winHeight && coachMeB > winHeight) {
         scroll.style.transform = `translate${check(dir)}(${100}%)`;
     }
-    if (skillsB < winHeight && projectsB > winHeight) {
+    if (coachMeB < winHeight && noiseControllerB > winHeight) {
         scroll.style.transform = `translate${check(dir)}(${200}%)`;
     }
-    if (projectsB < winHeight) {
+    if (noiseControllerB < winHeight && deforestDashB > winHeight) {
         scroll.style.transform = `translate${check(dir)}(${300}%)`;
+    }
+    if (deforestDashB < winHeight) {
+        scroll.style.transform = `translate${check(dir)}(${400}%)`;
     }
 };
